d81387f2d0d25c7425bb99cc1f19807632db50a4
Jack Thacker <jackthacker89@gmail.com>
2023-04-12 10:48:10 +0100

    Test drive up and down methods

--- thermostat.test.js
+++ thermostat.test.js
@@ -1,8 +1,20 @@
-const Thermostat = require('./thermostat');
+const Thermostat = require("./thermostat");
 
 describe("Thermostat class", () => {
   const thermostat = new Thermostat();
   it("starts with an initial temperature of 20 degrees", () => {
     expect(thermostat.getTemperature()).toBe(20);
-  })
-})
+  });
+  it("increases the temperature with an up method", () => {
+    thermostat.up(1);
+    expect(thermostat.getTemperature()).toBe(21);
+    thermostat.up(4);
+    expect(thermostat.getTemperature()).toBe(25);
+  });
+  it("decreases the temperature with a down method", () => {
+    thermostat.down(1);
+    expect(thermostat.getTemperature()).toBe(24);
+    thermostat.down(4);
+    expect(thermostat.getTemperature()).toBe(20);
+  });
+});
