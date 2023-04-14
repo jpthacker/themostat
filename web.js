95f9d1cc67aabf8f2187603ffb421c8021d97ce0
Jack Thacker <jackthacker89@gmail.com>
2023-04-14 11:38:13 +0100

    Add thermostat route methods
    Co-authored-by: Matt Parkinson <MGP1992@users.noreply.github.com>

---
+++ web.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const Thermostat = require('./thermostat');
+
+const thermostat = new Thermostat();
+
+app.get('/', (req, res) => {
+  res.send({message: 20})
+});
+
+app.get('/thermostat', (req, res) => {
+  res.send(`The thermostat's temperature is: ${thermostat.getTemperature()}.`)
+});
+
+app.post('/thermostat/up', (req, res) => {
+  thermostat.up(1);
+  res.send(`The thermostat's temperature has been increased by 1`);
+});
+
+app.post('/thermostat/down', (req, res) => {
+  thermostat.down(1);
+  res.send(`The thermostat's temperature has been decreased by 1`);
+});
+
+app.delete('/thermostat/reset', (req, res) => {
+  thermostat.resetTemperature();
+  res.send(`The thermostat's temperature has been reset`);
+})
+
+
+console.log(`Server listening on localhost:${port}`);
+app.listen(port);
